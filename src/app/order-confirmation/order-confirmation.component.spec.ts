e05d6f793cc057afacb47aafa6ec088498ee1328
hgirish <girish@post.com>
2018-04-12 20:51:12 -0700

    OrderConfirmation component specs passing

--- src/app/order-confirmation/order-confirmation.component.spec.ts
+++ src/app/order-confirmation/order-confirmation.component.spec.ts
@@ -1,12 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject
+} from '@angular/core/testing';
 
 import { OrderConfirmationComponent } from './order-confirmation.component';
 import { ShoppingCartService } from '../services/shopping-cart.service';
-const shoppingCartServiceSpy = jasmine.createSpyObj('ShoppingCartService', [
-  'empty'
-]);
+import { ProductsService } from '../services/products.service';
+import { DeliveryOptionsService } from '../services/delivery-options.service';
+import {
+  StorageService,
+  LocalStorageService
+} from '../services/storage.service';
+import sinon = require('sinon');
 
 describe('OrderConfirmationComponent', () => {
+  const shoppingCartServiceSpy = jasmine.createSpyObj('ShoppingCartService', [
+    'empty'
+  ]);
   let component: OrderConfirmationComponent;
   let fixture: ComponentFixture<OrderConfirmationComponent>;
 
@@ -30,4 +42,53 @@ describe('OrderConfirmationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});
+
+class MockShoppingCartService {
+  public emptyCalled = false;
+  public empty(): void {
+    this.emptyCalled = true;
+  }
+}
+
+describe('OrderConfirmationComponent', () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [OrderConfirmationComponent],
+        providers: [
+          {
+            provide: ProductsService,
+            useValue: sinon.createStubInstance(ProductsService)
+          },
+          {
+            provide: DeliveryOptionsService,
+            useValue: sinon.createStubInstance(DeliveryOptionsService)
+          },
+          { provide: StorageService, useClass: LocalStorageService },
+          { provide: ShoppingCartService, useClass: MockShoppingCartService }
+        ]
+      }).compileComponents();
+    })
+  );
+
+  it(
+    'should create the component',
+    async(() => {
+      const fixture = TestBed.createComponent(OrderConfirmationComponent);
+      const component = fixture.debugElement.componentInstance;
+      expect(component).toBeTruthy();
+    })
+  );
+
+  it(
+    'should call empty on shopping cart service when initialized',
+    async(
+      inject([ShoppingCartService], (service: MockShoppingCartService) => {
+        const fixture = TestBed.createComponent(OrderConfirmationComponent);
+        fixture.detectChanges();
+        expect(service.emptyCalled).toBeTruthy();
+      })
+    )
+  );
 });
