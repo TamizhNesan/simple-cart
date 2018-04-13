44d3ef69b6d12eecce0da59295e6b85ee32a518a
Girish <girish@post.com>
2018-04-13 13:02:27 -0700

    products.service.spec

--- src/app/route-guards/populated-cart-route.guard.spec.ts
+++ src/app/route-guards/populated-cart-route.guard.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed, async, inject } from '@angular/core/testing';
-
+import { MockBackend } from '@angular/http/testing';
 import { PopulatedCartRouteGuard } from './populated-cart-route.guard';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ShoppingCartService } from '../services/shopping-cart.service';
-const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
-const shoppingCartServiceSpy = jasmine.createSpyObj('ShoppingCartService', [
-  ''
-]);
+import { ShoppingCart } from '../models/shopping-cart.model';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import * as sinon from 'sinon';
+import { CartItem } from '../models/cart-item.model';
+
 describe('PopulatedCartRouteGuard', () => {
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+  const shoppingCartServiceSpy = jasmine.createSpyObj('ShoppingCartService', [
+    ''
+  ]);
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -24,4 +30,92 @@ describe('PopulatedCartRouteGuard', () => {
       expect(guard).toBeTruthy();
     })
   );
+});
+
+class MockShoppingCartService {
+  public unsubscribeCalled = false;
+  private subscriptionObservable: Observable<ShoppingCart>;
+  private subscriber: Observer<ShoppingCart>;
+  private cart: ShoppingCart = new ShoppingCart();
+
+  public constructor() {
+    this.subscriptionObservable = new Observable<ShoppingCart>(
+      (observer: Observer<ShoppingCart>) => {
+        this.subscriber = observer;
+        observer.next(this.cart);
+        return () => (this.unsubscribeCalled = true);
+      }
+    );
+  }
+
+  public get(): Observable<ShoppingCart> {
+    return this.subscriptionObservable;
+  }
+
+  public dispatchCart(cart: ShoppingCart): void {
+    this.cart = cart;
+    if (this.subscriber) {
+      this.subscriber.next(cart);
+    }
+  }
+}
+
+describe('PopulatedCartRouteGuard', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterModule],
+      providers: [
+        PopulatedCartRouteGuard,
+        { provide: ShoppingCartService, useClass: MockShoppingCartService },
+        { provide: Router, useValue: sinon.createStubInstance(Router) }
+      ]
+    });
+  });
+
+  it(
+    'should be injectable',
+    inject([PopulatedCartRouteGuard], (routeGuard: PopulatedCartRouteGuard) => {
+      expect(routeGuard).toBeTruthy();
+    })
+  );
+
+  describe('canActivate', () => {
+    it(
+      'should return true if there are items in the cart',
+      inject(
+        [Router, ShoppingCartService, PopulatedCartRouteGuard],
+        (
+          router: Router,
+          shoppingCartService: MockShoppingCartService,
+          grd: PopulatedCartRouteGuard
+        ) => {
+          // code
+          const newCart = new ShoppingCart();
+          const cartItem = new CartItem();
+          cartItem.quantity = 1;
+          newCart.items = [cartItem];
+          shoppingCartService.dispatchCart(newCart);
+          let x = grd.canActivate(null, null) as Observable<boolean>;
+
+          x.subscribe(result => expect(result).toBeTruthy());
+        }
+      )
+    );
+
+    it(
+      'should return false and redirect to "/" if there are no items in the cart',
+      inject(
+        [Router, PopulatedCartRouteGuard],
+        (router: Router, grd: PopulatedCartRouteGuard) => {
+          let x = grd.canActivate(null, null) as Observable<boolean>;
+          x.subscribe(result => expect(result).toBeFalsy());
+
+          sinon.assert.calledOnce(router.navigate as sinon.SinonSpy);
+          sinon.assert.calledWithExactly(router.navigate as sinon.SinonSpy, [
+            '/'
+          ]);
+        }
+      )
+    );
+  });
 });
--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,9 +1,23 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { async, TestBed, inject, fakeAsync } from '@angular/core/testing';
+import { Injectable, Injector } from '@angular/core';
 
 import { ProductsService } from './products.service';
-import { HttpClient } from '@angular/common/http';
-let httpClientSpy: { get: jasmine.Spy };
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  XHRBackend,
+  Response,
+  Http,
+  ResponseOptions,
+  ConnectionBackend,
+  RequestOptions,
+  BaseRequestOptions
+} from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Product } from '../models/product.model';
+import { Ingredient } from '../models/ingredient.model';
+
 describe('ProductsService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   beforeEach(() => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     TestBed.configureTestingModule({
@@ -24,3 +38,76 @@ describe('ProductsService', () => {
     })
   );
 });
+
+describe('ProductsService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [
+        ProductsService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
+    });
+  });
+
+  fit(
+    'should be injectable',
+    inject([ProductsService], (service: ProductsService) => {
+      console.log('product service expected tobe truthy');
+      expect(service).toBeTruthy();
+    })
+  );
+
+  describe('all()', () => {
+    fit(
+      'should call the correct http endpoint',
+      inject(
+        [ProductsService, XHRBackend],
+        (service: ProductsService, mockBackend: MockBackend) => {
+          console.log('mockbackend.connections' + mockBackend.connections);
+          mockBackend.connections.subscribe((conn: MockConnection) => {
+            console.log('conn: ' + conn.readyState);
+            this.lastConnection = conn;
+
+            console.log('last connection: ' + this.lastConnection);
+
+            expect(this.lastConnection.request.url).toEqual(
+              './assets/product.json'
+            );
+
+            this.lastConnection.mockRespond(
+              new Response(
+                new ResponseOptions({
+                  body: JSON.stringify(createProducts(2))
+                })
+              )
+            );
+          });
+
+          service.all().subscribe(products => {
+            console.log('service.all()');
+            expect(1).toEqual(products.length);
+
+            expect(products.length).toEqual(2);
+            expect(products[0].id).toBe('0');
+            expect(products[1].id).toBe('1');
+            console.log('product.length: ' + products.length);
+          });
+        } ///
+      )
+    );
+  });
+});
+
+function createProducts(count: number): Product[] {
+  const products = new Array<Product>();
+  for (let i = 0; i < count; i += 1) {
+    const product = new Product();
+    product.id = i.toString();
+    (product.name = `name ${i}`),
+      (product.price = i),
+      (product.ingredients = new Array<Ingredient>());
+    products.push(product);
+  }
+  return products;
+}
